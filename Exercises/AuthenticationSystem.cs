796e77c5bae104e4d9be6379da048d3c84985f32
LucasT <tressoldi.lucas@gmail.com>
2023-12-18 14:00:47 -0300

    Two final exercises

---
+++ Exercises/AuthenticationSystem.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+public class Authenticator
+{
+  private class EyeColor
+  {
+    public const string Blue = "blue";
+    public const string Green = "green";
+    public const string Brown = "brown";
+    public const string Hazel = "hazel";
+    public const string Brey = "grey";
+  }
+
+  public Authenticator(Identity admin) => this.admin = admin;
+
+  private Identity admin;
+
+  private IDictionary<string, Identity> developers
+      = new Dictionary<string, Identity>
+      {
+        ["Bertrand"] = new Identity
+        {
+          Email = "bert@ex.ism",
+          EyeColor = "blue"
+        },
+
+        ["Anders"] = new Identity
+        {
+          Email = "anders@ex.ism",
+          EyeColor = "brown"
+        }
+      };
+
+  public Identity Admin
+  {
+    get { return admin; }
+    set { admin = value; }
+  }
+
+  public IDictionary<string, Identity> GetDevelopers() => new ReadOnlyDictionary<string, Identity>(developers);
+}
+
+public struct Identity
+{
+  public string Email { get; set; }
+
+  public string EyeColor { get; set; }
+}
---
+++ Exercises/RedVsBlue-DarwinStyle.cs
@@ -0,0 +1,43 @@
+namespace RedRemoteControlCarTeam
+{
+  public class RemoteControlCar
+  {
+    public RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry, RunningGear runningGear) { }
+  }
+  public class RunningGear { }
+  public class Telemetry { }
+  public class Chassis { }
+  public class Motor { }
+}
+
+namespace BlueRemoteControlCarTeam
+{
+  public class RemoteControlCar
+  {
+    public RemoteControlCar(Motor motor, Chassis chassis, Telemetry telemetry) { }
+  }
+  public class Telemetry { }
+  public class Chassis { }
+  public class Motor { }
+}
+
+namespace Combined
+{
+  using Red = RedRemoteControlCarTeam;
+  using Blue = BlueRemoteControlCarTeam;
+  public static class CarBuilder
+  {
+    public static Red.RemoteControlCar BuildRed() => new Red.RemoteControlCar(
+            new Red.Motor(),
+            new Red.Chassis(),
+            new Red.Telemetry(),
+            new Red.RunningGear()
+        );
+
+    public static Blue.RemoteControlCar BuildBlue() => new Blue.RemoteControlCar(
+            new Blue.Motor(),
+            new Blue.Chassis(),
+            new Blue.Telemetry()
+        );
+  }
+}
